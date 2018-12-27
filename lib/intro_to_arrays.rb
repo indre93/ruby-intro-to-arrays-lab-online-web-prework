c496cdad4af33e910bfb7c6182513d737d5faf26
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-27 06:33:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,7 +2,7 @@ def instantiate_new_array
   []
 end
 
-def array_with_two_elements(my_two_array)
-  my_two_array = ["one", "two"]
-  puts my_two_array
+def array_with_two_elements(array)
+  array = ["one", "two"]
+  array
 end
